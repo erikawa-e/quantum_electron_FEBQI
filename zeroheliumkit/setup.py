2324642d5b3f4781179a8a242450d066942f2da1
riken EonHe <EHe>
2025-07-11 15:47:27 +0900

    with setup.py

---
+++ zeroheliumkit/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeroheliumkit',
+    version='0.4.1',
+    packages=find_packages(include=['src', 'helpers', 'fem', 'zeroheliumkit']), 
+    install_requires=[]
+)
